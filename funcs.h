33114046a0a3ec416c9699ab3049f8f495222725
Galieva Irina <Galieva.irinka@list.ru>
2017-12-18 18:02:59 +0300

    +slozheniye

--- funcs.h
+++ funcs.h
@@ -2,5 +2,6 @@
 
 #ifndef __FUNCS_H__
 #define __FUNCS_H__
+double MySum(double, double);
 
 #endif // __FUNCS_H__
--- program.cpp
+++ program.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 int main()
 {
+	cout << MySum(10, 3) << endl;
+	system("pause");
+return 0;
+
 }
 
 
